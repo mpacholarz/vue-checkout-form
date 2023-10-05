f17c981bfbf6f7145005e835b037142776bf8b21
Marcin Pacholarz <marrcin.pacholarz@gmail.com>
2023-10-05 10:38:37 +0200

    remove vue example

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
+/*eslint-env node*/
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: [],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
