175c1b7092fa76baef3c2867daa246871ac2970c
Marcin Pacholarz <marrcin.pacholarz@gmail.com>
2023-10-07 23:20:19 +0200

    add base input components

---
+++ src/components/base/BaseInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bg-white border-b-2 border-gray-300 rounded py-2 px-2.5">
+    <label :for="name" class="block text-xs font-medium text-gray-900">{{ label }}</label>
+    <input
+      type="text"
+      :name="name"
+      :id="name"
+      class="bg-transparent text-gray-900 text-base block w-full p-0"
+      :placeholder="placeholder"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  placeholder: {
+    type: String,
+    default: ''
+  },
+  name: {
+    type: String,
+    required: true
+  }
+})
+</script>
---
+++ src/components/base/BaseSelect.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bg-white border-b-2 border-gray-300 rounded py-2 px-2.5">
+    <label :for="name" class="block text-xs font-medium text-gray-900">{{ label }}</label>
+    <select
+      :id="name"
+      :name="name"
+      class="bg-transparent text-gray-900 text-base block w-full p-0 -ml-1 py-1"
+    >
+      <option disabled selected>{{ placeholder }}</option>
+      <option v-for="option in options" :value="option.value" :key="option.value">
+        {{ option.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  placeholder: {
+    type: String,
+    default: ''
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  options: {
+    type: Array<{ value: string; name: string }>,
+    default: () => []
+  }
+})
+</script>
