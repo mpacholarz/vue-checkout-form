5bc6c4b2dd25d2fc3b77cdf8e6b165f7f14845f4
Marcin Pacholarz <marrcin.pacholarz@gmail.com>
2023-10-07 23:20:04 +0200

    add base button component

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button type="button" :class="text ? 'btn-primary-text' : 'btn-primary'">
+      <slot name="icon"></slot>
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+defineProps({
+  text: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
